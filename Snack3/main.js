7b1f3ff28e83acba7f24ad4c008deec6d4ef70e6
Daniel <angelicodaniel93@gmail.com>
2022-10-26 15:44:08 +0200

    inizio snack3

---
+++ Snack3/main.js
@@ -0,0 +1,14 @@
+/* Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array,
+ognuno formato da 10 numeri casuali da 1 a 100.
+Ogni volta che ne crei uno, stampalo. */
+
+//fai inserire un numero all'utente, che chiameremo N
+let N = parseInt(prompt('inserisci un numero'))
+
+//genera N array
+let x = 0
+
+while (x < N) {
+
+}
